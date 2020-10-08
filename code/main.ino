ca2d27e8c6c216e657eb76f92b7d21629be06fd2
D3cl4n <38691337+D3cl4n@users.noreply.github.com>
2020-10-07 20:06:43 -0400

    Update main.ino

--- code/main.ino
+++ code/main.ino
@@ -1,9 +1,42 @@
-void setup() {
-  // put your setup code here, to run once:
+#include "interface.h"
 
+void setup()
+{
+	
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+	int bpm = 60;
 
+	Note qnC;
+	qnC.noteValue = (60 / bpm) * 1000;
+	qnC.noteName = "C";
+
+	Note qnB;
+	qnB.noteValue = (60 / bpm) * 1000;
+	qnB.noteName = "b";
+
+	Note enA;
+	enA.noteValue = (60 / (bpm * 2)) * 1000;
+	enA.noteName = "a"
+
+	Note enG;
+	enG.noteValue = (60 / (bpm * 2)) * 1000;
+	enG.noteName = "g";
+
+	Note qnF;
+	qnF.noteValue = (60 / bpm) * 1000;
+	qnF.noteName = "f";
+
+	Note qnE;
+	qnE.noteValue = (60 / bpm) * 1000;
+	qnE.noteName = "e";
+
+	Note qnD;
+	qnD.noteValue = (60 / bpm) * 1000;
+	qnD.noteName = "d";
+
+
+	uint16_t firstSong[58] = {}	// joy to world
 }
