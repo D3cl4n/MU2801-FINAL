bae2781a178d1a67ad6704523fca0e80acc195fa
evelyntrvn <60491379+evelyntrvn@users.noreply.github.com>
2020-10-11 10:34:55 -0400

    Update main.ino

--- code/main.ino
+++ code/main.ino
@@ -1,55 +1,87 @@
-#include "interface.h"
+/*
+Final Project
+*/
 
-void setup()
-{
-	
+//solenoid pins
+int do1 = ;
+int re = ;
+int mi = ;
+int fa = ;
+int so = ;
+int la = ;
+int ti = ;
+int do2 = ;
+
+int ontime = 50;
+
+
+void setup() {
+    pinMode(do1, OUTPUT);
+    pinMode(re, OUTPUT);
+    pinMode(mi, OUTPUT);
+    pinMode(fa, OUTPUT);
+    pinMode(so, OUTPUT);
+    pinMode(la, OUTPUT);
+    pinMode(ti, OUTPUT);
+    pinMode(do2, OUTPUT);
+    
+    while (!Serial);
+    
+    Serial.begin(9600);
 }
 
-void playSong(uint16_t song[58])
-{
-	for (int i = 0; i < 58; i++)
-	{
-		if (song[i].noteValue == ... && song[i].noteName = ...)
-		{
-			// play note here	
-		}
-	}
+void play(byte note, byte velocity){
+    int dynamic = (velocity/127)*50;
+    
+    if (note == 0){
+        digitalWrite(do1, HIGH);
+        delay(dynamic);
+        digitalWrite(do1, LOW);
+        delay(ontime);
+    }else if (note == 2){
+        digitalWrite(do1, HIGH);
+        delay(dynamic);
+        digitalWrite(do1, LOW);
+        delay(ontime);
+    }else if (note == 4){
+        digitalWrite(do1, HIGH);
+        delay(dynamic);
+        digitalWrite(do1, LOW);
+        delay(ontime);
+    }else if (note == 5){
+        digitalWrite(do1, HIGH);
+        delay(dynamic);
+        digitalWrite(do1, LOW);
+        delay(ontime);
+    }else if (note == 7){
+        digitalWrite(do1, HIGH);
+        delay(dynamic);
+        digitalWrite(do1, LOW);
+        delay(ontime);
+    }else if (note == 9){
+        digitalWrite(do1, HIGH);
+        delay(dynamic);
+        digitalWrite(do1, LOW);
+        delay(ontime);
+    }else if (note == 11){
+        digitalWrite(do1, HIGH);
+        delay(dynamic);
+        digitalWrite(do1, LOW);
+        delay(ontime);
+    }else if (note == 12){
+        digitalWrite(do1, HIGH);
+        delay(dynamic);
+        digitalWrite(do1, LOW);
+        delay(ontime);
+    }
 }
 
-void loop()
-{
-	int bpm = 60;
-
-	Note qnC;
-	qnC.noteValue = (60 / bpm) * 1000;
-	qnC.noteName = "C";
-
-	Note qnB;
-	qnB.noteValue = (60 / bpm) * 1000;
-	qnB.noteName = "b";
-
-	Note enA;
-	enA.noteValue = (60 / (bpm * 2)) * 1000;
-	enA.noteName = "a"
-
-	Note enG;
-	enG.noteValue = (60 / (bpm * 2)) * 1000;
-	enG.noteName = "g";
-
-	Note qnF;
-	qnF.noteValue = (60 / bpm) * 1000;
-	qnF.noteName = "f";
-
-	Note qnE;
-	qnE.noteValue = (60 / bpm) * 1000;
-	qnE.noteName = "e";
-
-	Note qnD;
-	qnD.noteValue = (60 / bpm) * 1000;
-	qnD.noteName = "d";
-
-
-	uint16_t firstSong[58] = {}	// joy to world
-	
-	playSong(firstSong);
+void loop() {
+    if (Serial.available>2){
+      byte note = Serial.read(); 
+      byte velocity = Serial.read();
+      if (velocity>0){
+        play(note, velocity);
+      }
+    }
 }
